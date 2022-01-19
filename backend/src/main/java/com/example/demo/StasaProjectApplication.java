9756887a2bce78056ceb4f8b99da30de31663727
Martin Jönsson <mr_tines_j@hotmail.com>
2022-01-19 11:23:19 +0100

    Unfinished firebase integration

--- backend/src/main/java/com/example/demo/StasaProjectApplication.java
+++ backend/src/main/java/com/example/demo/StasaProjectApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class StasaProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StasaProjectApplication.class, args);
+		UserService userService = new UserService();
+		System.out.println(userService.getUserCollection());
 	}
-
 }
