23de7bf82e98bdf2e369aef72002e718904a5fe7
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2022-01-19 09:27:57 +0100

    Test class basic setup

---
+++ backend/src/test/java/com/example/demo/ServerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ServerTest {
+
+    @Test
+    public void testJunit() {
+        String name1 = "Sebbe";
+        assertNotNull(name1);
+        assertEquals(name1, "Sebbe");
+    }
+}
