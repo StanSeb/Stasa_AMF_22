5800bb0b3badad4bd85a9007e2051cf2ed1d7fa4
Martin Jönsson <mr_tines_j@hotmail.com>
2022-01-20 15:53:55 +0100

    POST/GET inlägg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
 
     this.state = {
       loggedInUser: null,
+      thread: {title: "", content: "", username: ""},
     };
   }
 
@@ -86,13 +87,29 @@ class App extends Component {
     }
   }
 
+  async componentDidMount() {
+    //GET THIS SHIT!
+    let token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjQwMTU0NmJkMWRhMzA0ZDc2NGNmZWUzYTJhZTVjZDBlNGY2ZjgyN2IiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc3Rhc2EtZGEzZDUiLCJhdWQiOiJzdGFzYS1kYTNkNSIsImF1dGhfdGltZSI6MTY0MjY4ODIxNSwidXNlcl9pZCI6InJZNHRUS2VEQUViRkhReXZRUUhiNXBIbXllVjIiLCJzdWIiOiJyWTR0VEtlREFFYkZIUXl2UVFIYjVwSG15ZVYyIiwiaWF0IjoxNjQyNjg4MjE1LCJleHAiOjE2NDI2OTE4MTUsImVtYWlsIjoibWFydGluLncuam9uc3NvbkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsibWFydGluLncuam9uc3NvbkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.ibAUW1_bf1cdkf_WZOIx4mlSsyK5rnrzWv4N0SQV2QKf0Jq8CqhyTQhQHb3h3LvXsXKF1ZuMsLXKXnubU_FRE9jnZgLDXXI_LLP3EGJvn870Bl0HSIS6_QI4yZmSc1-UUsSrD68X_9lDzbdiGM-ccA0x1bXFzbrc_xovKPbbbYSfUULWgMVVlc2VcfXT9vNScu7n6_G9vVGm6_mgiaSzuUEt5CU16_heF0DLi4oVr4KvS8b47iwPLx2EgS13lV7Maz8UcbgeKwmKVaMqpJg81tWvgy9JBKdfH7fVKf6BkpQEc-OmtwjmD0Unl0_X0qy0QpLG0x7Z-D7CZOG9EIJowA";
+    const config = {
+      headers: {Authorization: "Bearer " + token}
+    }
+    await Axios.get('http://localhost:8080/threads/getforuser?userId=iey6vYYcnEerpyyKCVHGE6vGyss2', config)
+      .then(response => response.data)
+      .then((data) => {
+        let title = data[0].title;
+        let content = data[0].content;
+        let username = data[0].user_id;
+        this.setState({ thread: {title: title, content: content, username: username} })})
+  }
+
   render() {
     return (
       <Router>
         
 
       <div className="App">
-        <Thread/>
+        <Thread thread={{title: this.state.thread.title, username: this.state.thread.username, content: this.state.thread.content}}/>
+        <NewThread/>
         <div id="login">
           <label>Email</label>
           <input id="email" type="text" />
