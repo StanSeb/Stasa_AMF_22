23f093bf660511ef9380b06595d86478d1c355c1
Philip <philip@alloneapp.com>
2022-01-27 04:02:10 -0600

    Update application.properties

--- backend/src/main/java/com/stasa/StasaApplication.java
+++ backend/src/main/java/com/stasa/StasaApplication.java
@@ -8,16 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StasaApplication {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD:backend/src/main/java/com/stasa/StasaProjectApplication.java
-		SpringApplication.run(StasaProjectApplication.class, args);
-		// UserService userService = new UserService();
-		 //System.out.println(userService.getUserCollection());
-		System.out.println("Nu tar vi helg");
-=======
 		SpringApplication.run(StasaApplication.class, args);
 
 		System.out.println("Here we go again... ¯\\_(ツ)_/¯");
-
->>>>>>> EmailAuth:backend/src/main/java/com/stasa/StasaApplication.java
 	}
 }
