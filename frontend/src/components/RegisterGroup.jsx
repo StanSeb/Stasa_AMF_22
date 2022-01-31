880fa3405a0d29351545d7b4e562ea6b16321ddf
Abbas <contactabbas07@gmail.com>
2022-01-31 12:06:28 +0100

    Post/Get groups frontend complete.

--- frontend/src/components/RegisterGroup.jsx
+++ frontend/src/components/RegisterGroup.jsx
@@ -9,7 +9,7 @@ export default class RegisterGroup extends Component {
 				title: "",
 				description: "",
 				user:{
-          id: 7}
+          id: ''}
         
 				
 			},
@@ -29,31 +29,24 @@ export default class RegisterGroup extends Component {
   }
 
   registerGroup(){
-
-
-		let group = {
+    let group = {
 			title: this.state.title,
 			description: this.state.description,
+      user:{
+        id: 7}
 		};
-
-    this.setState({ group }, () => {
-      console.log(this.state.group)
-			axios
-				.post("http://localhost:8080/register/group", this.state.group)
-        
-				.then((response) => {
-					console.log(response);
-				})
-				.catch((error) => {
-					console.log(error);
-				});
-		});
-
+    
+     fetch ("http://localhost:8080/register/group", {
+       method: 'POST',
+       headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(group)})
+      console.log(this.state.group);
+  
   }
 
   render() { 
     return (
-    <>
+    
       <div className="newGroup">
         <label>Title: </label> 
         <input type="text" value={this.state.title} onChange={this.titleChange}/>
@@ -61,6 +54,5 @@ export default class RegisterGroup extends Component {
         <input type="text" value={this.state.description} onChange={this.descriptionChange}/>
         <button onClick={() => this.registerGroup()}>Register Group</button>
       </div>
-    </>
   )}
 }
