c1c51033301384fae856d35d0f6e3f76d66c01f9
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2022-01-31 13:26:31 +0100

    Fix commit after merge

---
+++ backend/src/main/java/com/stasa/StasaApplication.java
@@ -0,0 +1,14 @@
+package com.stasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StasaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StasaApplication.class, args);
+
+        System.out.println("Here we go again... ¯\\_(ツ)_/¯");
+    }
+}
