ae1d8643ac134e47efb5ad83f8285d0f9388c3da
Martin Jönsson <mr_tines_j@hotmail.com>
2022-02-01 13:18:06 +0100

    Start of branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
-import "./index.css"
-import Navbar from './components/Navbar'
+import "./index.css";
+import Navbar from "./components/Navbar";
 import GroupPage from "./pages/GroupPage";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			loggedInUser: { id: 7, username: "Leif Bond", privilege: "admin" },
+			userID: 0,
 			group: {
 				id: 1,
 				name: "Bonds ''Speciella'' Klubb",
 				info: "Här pratar vi bara om hemliga agenter",
 				admin: "Leif Bond",
 			},
-			thread: "",
 		};
+		this.handleWhoAmI = this.handleWhoAmI.bind(this);
 	}
 
-  constructor(){
-    super();
-    this.state = {userID: 0} 
-    this.handleWhoAmI = this.handleWhoAmI.bind(this)
-  }
+	handleWhoAmI(id) {
+		this.setState({ userID: id }, () => {
+			console.log(this.state.userID);
+		});
+	}
 
-  handleWhoAmI(id){
-    this.setState({userID: id}, ()=> {
-      console.log(this.state.userID)
-    })
-  }
-
-  render() {
-    return (
-      <Navbar storeId={this.handleWhoAmI} userObj={this.state.userID}/>
-    )
-  }
+	render() {
+		return (
+			<>
+				<Navbar storeId={this.handleWhoAmI} userObj={this.state.userID} />
+				<GroupPage group={this.state.group} loggedInUser={this.state.userID} />
+			</>
+		);
+	}
 }
 export default App;
