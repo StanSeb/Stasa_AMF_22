91a35424b94f3b99387519cbfe4ea7a14b63b49f
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2022-02-03 13:35:28 +0100

    Merge fix
    ProfilePage

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,14 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import { unstable_renderSubtreeIntoContainer } from 'react-dom';
-import {useNavigate} from 'react-router-dom'
-import axios from 'axios';
+import { render } from 'react-dom';
+import { useNavigate } from 'react-router-dom'
 
 export default function Profile(props) {
-   async function terminateUserById() {
-        await axios.put("/auth/terminateUser/"+ props.userObj.id)
-        .then(response => {
-            alert(response.data)
-        })
+    async function terminateUserById() {
+        await axios.put("/auth/terminateUser/" + props.userObj.id)
+            .then(response => {
+                alert(response.data)
+            })
     }
     async function logOut() {
         await fetch("/logout", {
@@ -24,41 +23,30 @@ export default function Profile(props) {
 
     let navigate = useNavigate();
 
-    return <div className='profileContainer'>
-        <div> Welcome to profile </div> 
-        <button onClick={()=> {
+    return (
+    <div className='profileContainer'>
+        <div> Welcome to profile </div>
+        <button onClick={() => {
             navigate("/createGroup");
-        }}>Create a group</button>  
-        <button onClick={()=> {
+        }}>Create a group</button>
+        <button onClick={() => {
             navigate("/getGroups");
-        }}>Get groups</button> 
+        }}>Get groups</button>
+        
+        {/* TODO: Fixa så funktion för denna */}
+        {/* <div>
+            <div>{this.state.groups.map((group) => (
+                <ul key={group.id}>
+                    <li> Title: <span>{group.group.title}</span> <br />
+                        Description: <span>{group.group.description}</span> <br />
+                        Group role: <span>{group.memberRoles.title}</span>
 
+                    </li>
+                </ul>
+            ))}</div>
+        </div> */}
         <button onClick={terminateUserById}>Stäng av kontot</button>
         <button onClick={logOut}>Logga ut</button>
-        </div>;
-
-  
-}
-
-
-
-  render() {
-
-    return <div>PROFILE PAGE
-
-<div>{this.state.groups.map((group) =>( 
-            <ul key={group.id}> 
-             <li> Title: <span>{group.group.title}</span> <br />
-                  Description: <span>{group.group.description}</span> <br />
-                  Group role: <span>{group.memberRoles.title}</span>
-
-               </li>
-            </ul>  
-            ))}</div>
-
-    </div>;
-
-
-
-}
+    </div>
+    )
 }
