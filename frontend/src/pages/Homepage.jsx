e9c24e7c10b79612871529c72dcff6070cdb03b3
Sebastian Stanczak <stanczak.sebastian@gmail.com>
2022-02-14 14:42:17 +0100

    homepage css shenanigans

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default class Homepage extends Component {
   constructor(props) {
@@ -14,30 +14,30 @@ export default class Homepage extends Component {
 
   componentDidMount() {
     axios
-    .get("http://localhost:8080/rest/groups/getAllActiveGroups")
+      .get("http://localhost:8080/rest/groups/getAllActiveGroups")
       .then((response) => response.data)
       .then((data) => {
         this.setState({ groups: data });
       });
   }
 
- 
-render() {
 
-    return <div>Homepage
+  render() {
 
-      <div>{this.state.groups.map((group) => (
-        <ul key={group.id}>
-          <li> <Link to={`/group/${group.id}`}><span>{group.title}</span></Link>:<br />
+    return <div className='homepage-container'>
+      <div className="homepage-title">
+        <h2>Forumets grupper</h2>
+      </div>
+      <ul className='list-container'>{this.state.groups.map((group) => (
+          <li key={group.id}> <Link to={`/group/${group.id}`}><span>{group.title}</span></Link>:<br />
             Description: <span>{group.description}</span> <br />
           </li>
-        </ul>
-      ))}</div>
+      ))}</ul>
 
     </div>;
 
 
 
-  
-}
+
+  }
 }
